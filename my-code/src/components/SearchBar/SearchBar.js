d3400d21288ef4eab8f9d179a73c09376c68fd18
stanisze <oskar.staniszewski@nokia.com>
2021-03-17 22:42:11 +0100

    generating search proper path on keyDown event

--- my-code/src/components/SearchBar/SearchBar.js
+++ my-code/src/components/SearchBar/SearchBar.js
@@ -1,7 +1,19 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import history from '../../config/history'
 const SearchBar = () => { 
-  return <div className='search-bar'><input title='What are you looking for?'></input></div>
+  const [searchInput, setSearchInput] = useState('');
+  const getSearchInput = event => setSearchInput(event.target.value);
+  console.log(searchInput)
+
+  const onKeyDown = event => {
+    if(event.key === 'Enter') {
+      history.push({
+        pathname:'/search',
+        search: `?title=${searchInput}`
+      })
+    }
+  }
+  return <div className='search-bar'><input title='What are you looking for?' onKeyDown={event => onKeyDown(event)} onChange={event => getSearchInput(event)}></input></div>
 }
 
 export default SearchBar;
