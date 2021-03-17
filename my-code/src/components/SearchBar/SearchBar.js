a7213f226cff902d72a6110fde587d024a6efde6
stanisze <oskar.staniszewski@nokia.com>
2021-03-17 21:34:55 +0100

    SearchBar

---
+++ my-code/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SearchBar = () => { 
+  return <div className='search-bar'><input title='What are you looking for?'></input></div>
+}
+
+export default SearchBar;
