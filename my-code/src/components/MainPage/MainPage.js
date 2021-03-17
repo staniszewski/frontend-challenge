7a13a06a2762df1ee9c581a15a60e067b1e008a1
stanisze <oskar.staniszewski@nokia.com>
2021-03-17 21:34:14 +0100

    MainPage with SearchBar

---
+++ my-code/src/components/MainPage/MainPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import SearchBar from '../SearchBar/SearchBar'
+
+const MainPage = () => {
+
+  return <><h1>Hi im searching</h1><SearchBar /></>
+}
+
+export default MainPage;
