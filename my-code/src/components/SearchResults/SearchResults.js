512cb0d049cdf8453e619dd4332c7fc488b901a6
stanisze <oskar.staniszewski@nokia.com>
2021-04-09 23:03:19 +0200

    set current search from url; first diplay results

--- my-code/src/components/SearchResults/SearchResults.js
+++ my-code/src/components/SearchResults/SearchResults.js
@@ -4,17 +4,36 @@ import axios from 'axios';
 import { getMoviesByTitle } from '../../config/endpointConfig'
 import icon from '../../layout/icon-arrow-grey.svg'
 import { AppContext } from '../Store/Store';
+import DispatchActions from '../Store/DispatchActions'
 
-
+const { UPDATE_SEARCH_VALUE } = DispatchActions;
 const SearchResults = () => {
   const [results, setResults] = useState([])
   const [state, dispatch] = useContext(AppContext);
+  const { search } = useLocation();
+  const currentSearch = new URLSearchParams(search).get('title')
+  
   useEffect( ()=> {
-    async function fetchData(){await axios.get(getMoviesByTitle() + state.search.searchValue).then(setResults)};
+    if(state.search.searchValue !== currentSearch) {
+      dispatch({type: UPDATE_SEARCH_VALUE, payload: currentSearch})
+    } 
+    const fetchData = async () => {
+      const { data: response } = await axios.get(getMoviesByTitle() + currentSearch)
+      response.Response === "True" && setResults(response.Search) 
+    };
     fetchData();
   }, [])
   
-  return <div className="search-results"><h1><img src={icon}></img>Search results page: under construction</h1></div>
+console.log(results)
+  return <div className="search-results"><h1><img src={icon}></img>Search results page: under construction</h1>
+  <ul>
+    { results.map((result, index) => 
+      <li key={index}>
+          {JSON.stringify(result)}
+      </li>
+      )}
+  </ul>
+  </div>
 }
 
 export default SearchResults;
