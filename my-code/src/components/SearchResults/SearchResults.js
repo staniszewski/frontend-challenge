6ee62ce0c29f2be9cef248a42d8da674472cc159
stanisze <oskar.staniszewski@nokia.com>
2021-04-09 22:30:38 +0200

    reading search term from store

--- my-code/src/components/SearchResults/SearchResults.js
+++ my-code/src/components/SearchResults/SearchResults.js
@@ -1,15 +1,20 @@
-import React, {useEffect, useState}   from 'react';
+import React, {useContext, useEffect, useState}   from 'react';
+import { useLocation, useParams } from 'react-router';
 import axios from 'axios';
 import { getMoviesByTitle } from '../../config/endpointConfig'
+import icon from '../../layout/icon-arrow-grey.svg'
+import { AppContext } from '../Store/Store';
+
 
 const SearchResults = () => {
   const [results, setResults] = useState([])
+  const [state, dispatch] = useContext(AppContext);
   useEffect( ()=> {
-    async function fetchData(){await axios.get(getMoviesByTitle()+ 'why').then(setResults)};
+    async function fetchData(){await axios.get(getMoviesByTitle() + state.search.searchValue).then(setResults)};
     fetchData();
   }, [])
-  console.log(results.data)
-  return <div className="search-results"><h1>Search results page: under construction</h1></div>
+  
+  return <div className="search-results"><h1><img src={icon}></img>Search results page: under construction</h1></div>
 }
 
 export default SearchResults;
